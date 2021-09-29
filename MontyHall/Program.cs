00bb0b059b7711e480c4868bb91582b2cd7e6a6a
gy2006 <32008001@qq.com>
2021-09-29 20:49:12 +0200

    read settings from stdin

--- MontyHall/Program.cs
+++ MontyHall/Program.cs
@@ -6,7 +6,51 @@ namespace MontyHall
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Monty Hall Problem");
+            
+            Console.WriteLine(">>> Please entry number of games:");
+            var numOfGames = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(">>> Please entry whether you change the door or not (true/false):");
+            var isChangeDoor = bool.Parse(Console.ReadLine());
+        
+            Run(numOfGames, isChangeDoor);
+        }
+
+        private static void Run(int numOfGames, bool isChangeDoor)
+        {
+            var numOfSuccess = 0;
+            
+            for (var i = 0; i < numOfGames; i++)
+            {
+                var game = new Game(isChangeDoor);
+                PrintInitStatus(game);
+                game.PlayerSelect(door => Console.WriteLine("[Player] select door {0}", door.Number));
+                game.PresenterOpen(door => Console.WriteLine("[Presenter] open door {0}", door.Number));
+                var isCar = game.PlayerOpen(door => Console.WriteLine("[Player] open door {0}", door.Number));
+
+                var output = "Failed";
+                if (isCar)
+                {
+                    numOfSuccess++;
+                    output = "Success";
+                }
+
+                Console.WriteLine("-------- {0} --------", output);
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine(">>>>>>>>>>> Success Rate is {0}% >>>>>>>>>>>", numOfSuccess / (double) numOfGames * 100 );
+        }
+
+        private static void PrintInitStatus(Game game)
+        {
+            Console.Write("--- ");
+            foreach (var door in game.Doors)
+            {
+                Console.Write("{0}={1} ", door.Number, door.Behind);
+            }
+            Console.WriteLine(" ---");
         }
     }
 }
